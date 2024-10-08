2033d782c652d184c452c50eca9f563a51a91b93
Fury-Senpai <143637458+Fury-Senpai@users.noreply.github.com>
2024-10-08 23:22:31 +0530

    Add files via upload

---
+++ arrayIntersection.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+void intersectionArray(int arr[], int arr2[],int size1 , int size2,){
+    for(int i = 0; i<size1; i++){
+        if(arr[i]==arr[j]
+    }
+}
+
+int main(){
+    int arr[5] = {1,2,3,4,5};
+    int arr2[5] = {1,2,6,7,8};
+}
